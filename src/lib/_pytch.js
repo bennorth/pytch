d18c1384f00bc713bea44a3a30949c4bff838e1a
Ben North <ben@redfrontdoor.org>
2019-06-22 07:45:00 +0100

    Add start of green-flag state machine in JS-land

--- src/lib/_pytch.js
+++ src/lib/_pytch.js
@@ -5,6 +5,14 @@ var $builtinmodule = function (name) {
 
     mod.hello_world = Sk.builtin.str("hello, world");
 
+    // The green flag will go through three states.  It starts off in
+    // 'not-clicked-yet'.  When processing the first frame after it has been
+    // clicked it's in 'just-clicked', and for all subsequent frames it is in
+    // 'has-been-clicked'.  Only one pass through this set of transitions is
+    // possible.
+    mod.green_flag_elt = document.getElementById("green-flag");
+    mod.green_flag_state = "not-clicked-yet";
+
     mod.frame_idx = 0;
     mod.frame_idx_elt = document.getElementById("frame-idx");
 
