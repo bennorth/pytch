25190e8da7f24128a29024aa516f8b509e8ce34f
Ben North <ben@redfrontdoor.org>
2019-06-22 08:21:14 +0100

    handlers: Add at Python level
    This will store all registered handlers.  Initially just green-flag ones.

--- src/lib/pytch.py
+++ src/lib/pytch.py
@@ -1,3 +1,13 @@
 import _pytch
 
 from _pytch import hello_world, run
+
+
+# List of event-handlers, each represented as a 2-tuple
+# (event-descriptor, handler-function).
+#
+# The 'event-descriptor' is a string, whose first character specifies
+# what type of event this handler is for:
+#
+#   G --- green flag clicked
+handlers = []
