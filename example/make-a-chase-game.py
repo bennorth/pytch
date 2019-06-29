51d2f57f4588ee7f36938839cd90dfe3f056e5eb
Ben North <ben@redfrontdoor.org>
2019-06-29 09:11:44 +0100

    Add 'make a chase game' example
    Does not work yet.

---
+++ example/make-a-chase-game.py
@@ -0,0 +1,39 @@
+import pytch
+
+class Player(pytch.Sprite):
+    Costumes = {'python': ('pytch-images/python-logo.png', 25, 25)}
+    Sounds = {'pop': 'pytch-audio/pop.mp3'}
+
+    def __init__(self):
+        pytch.Sprite.__init__(self)
+        self.score = 0
+
+    @when_key_pressed('ArrowRight')
+    def move_right(self):
+        self.change_x_pos(10)
+
+    @when_key_pressed('ArrowLeft')
+    def move_right(self):
+        self.change_x_pos(-10)
+
+    @when_key_pressed('ArrowUp')
+    def move_right(self):
+        self.change_y_pos(10)
+
+    @when_key_pressed('ArrowDown')
+    def move_right(self):
+        self.change_y_pos(-10)
+
+    @when_green_flag_clicked
+    def play_game(self):
+        pytch.show_variable(self, 'score')
+        while True:
+            if self.touching('Star'):
+                self.score += 1
+                pytch.play_sound_until_done('pop')
+
+class Star(pytch.Sprite):
+    @when_green_flag_clicked
+    def play_game(self):
+        while True:
+            self.glide_to('.random', 1.0)
