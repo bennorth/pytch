76585d7195ff9aa6c40c7aba186657abbab3d126
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2019-07-03 15:35:30 +0100

    Simple script to publish to production server

---
+++ example/pytch/publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script to publish the Pytch examples to pytch.org
+# Assumes you have passwordless logins set up.
+
+# Run this from the base Pytch directory
+# e.g. examples/pytch/publish.sh
+
+SERVER=pytch.org
+PUBLISHDIR=pytch.org/prototype
+
+scp dist/skulpt-stdlib.js dist/skulpt.min.js $SERVER:$PUBLISHDIR/dist/
+scp example/pytch-images/ example/pytch-audio/ $SERVER:$PUBLISHDIR/
+scp example/pytch.html $SERVER:$PUBLISHDIR/
+scp example/*.py $SERVER:$PUBLISHDIR/
