bd47cb1dc9a461f6648c51a4b13398d032142663
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2019-07-03 16:49:16 +0100

    Have publication script rewrite the path to Skulpt on the remote file

--- example/pytch/publish.sh
+++ example/pytch/publish.sh
@@ -6,10 +6,14 @@
 # Run this from the base Pytch directory
 # e.g. examples/pytch/publish.sh
 
+SSHOPTS=-C
 SERVER=pytch.org
 PUBLISHDIR=pytch.org/prototype
 
-scp dist/skulpt-stdlib.js dist/skulpt.min.js $SERVER:$PUBLISHDIR/dist/
-scp -r example/pytch-images/ example/pytch-audio/ $SERVER:$PUBLISHDIR/
-scp example/pytch.html $SERVER:$PUBLISHDIR/
-scp example/*.py $SERVER:$PUBLISHDIR/
+scp $SSHOPTS dist/skulpt-stdlib.js dist/skulpt.min.js $SERVER:$PUBLISHDIR/dist/
+scp $SSHOPTS -r example/pytch-images/ example/pytch-audio/ $SERVER:$PUBLISHDIR/
+scp $SSHOPTS example/pytch.html $SERVER:$PUBLISHDIR/
+# Adjust the Skulpt file paths on published site
+ssh $SSHOPTS pytch.org sed -i 's/\\\.\\\.\\/dist/dist/g' $PUBLISHDIR/pytch.html
+ssh $SSHOPTS pytch.org ls $PUBLISHDIR
+scp $SSHOPTS example/*.py $SERVER:$PUBLISHDIR/
