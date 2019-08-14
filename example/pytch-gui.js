d53ed313a418c8dc26d7586a25ad28e7f0dddd7a
Ben North <ben@redfrontdoor.org>
2019-08-14 22:32:10 +0100

    Populate 'Examples' menu on load

--- example/pytch-gui.js
+++ example/pytch-gui.js
@@ -1,2 +1,39 @@
 $(document).ready(function() {
+
+    ////////////////////////////////////////////////////////////////////////////////
+    //
+    // Populate 'Examples' menu
+
+    var examples_menu = $("#editor-menubar");
+
+    var examples = [
+        {label: 'Pong', url: 'pong.py'},
+        {label: 'Chase Game', url: 'make-a-chase-game.py'},
+        {label: 'Space Invaders', url: 'space-invaders.py'},  // Doesn't exist yet.
+    ];
+
+    function load_example(evt) {
+        examples_menu.jqDropdown("hide");
+
+        var editor = ace.edit("editor");
+        var code_url = evt.target.dataset.pytchUrl;
+        var project_name = evt.target.dataset.pytchLabel;
+        $.ajax(code_url).then(code_text => {
+            $("#user-chosen-project-name").val("My " + project_name);
+            editor.setValue(code_text);
+        });
+    };
+
+    var examples_menu_contents = $('#jq-dropdown-examples > ul');
+
+    examples.forEach((example) => {
+        var label_elt = $("<li><label"
+                          + " data-pytch-url=\"" + example.url + "\""
+                          + " data-pytch-label=\"" + example.label + "\">"
+                          + example.label
+                          + "</label></li>");
+        $(label_elt).click(load_example);
+        examples_menu_contents.append(label_elt);
+    });
+
 });
