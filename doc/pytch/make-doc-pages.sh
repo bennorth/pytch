2579f695847e81b0162d00ab9baceec86d64405e
Ben North <ben@redfrontdoor.org>
2019-08-20 06:53:25 +0100

    Add titles to Help and About pages

--- doc/pytch/make-doc-pages.sh
+++ doc/pytch/make-doc-pages.sh
@@ -5,12 +5,12 @@
 
 render_html() {
     (
-        cat pytch-page-header.html
+        sed "s/TITLE-GOES-HERE/$3/" < pytch-page-header.html
         pandoc --quiet -f gfm -t html5 "$1"
         cat pytch-page-footer.html
     ) \
         > "$2"
 }
 
-render_html help.md help.html
-render_html about.md about.html
+render_html help.md help.html "Pytch: Help"
+render_html about.md about.html "Pytch: About"
