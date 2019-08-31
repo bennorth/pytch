044bfa73e88e4b0e4a0b31cb2357e42e282d6baf
Ben North <ben@redfrontdoor.org>
2019-08-31 12:34:28 +0100

    Add smoke test driver for Mocha
    (Quite likely that the boilerplate from this will be extracted into JS
    modules in due course.)

---
+++ test/pytch/test_project.js
@@ -0,0 +1,39 @@
+"use strict";
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+const assert = require("assert");
+const fs = require("fs");
+
+// Inject 'Sk' object into global namespace.
+const reqskulpt = require('../../support/run/require-skulpt').requireSkulpt;
+this.Sk = reqskulpt(false);
+
+// Connect read/write to filesystem and stdout.
+Sk.configure({
+    read: (fname) => { return fs.readFileSync(fname, "utf8"); },
+    output: (args) => { process.stdout.write(args); },
+});
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Test helpers
+
+const import_from_local_file = function(fname) {
+    var code_text = fs.readFileSync(fname, { encoding: "utf8" });
+    return Sk.importMainWithBody(fname, false, code_text, true);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Module 'pytch.project'
+
+describe("pytch.project module", function() {
+    it("can be imported", function() {
+        var import_result = import_from_local_file("py/project/just_import.py");
+        assert.ok(import_result.$d.pytch_project);
+    });
+});
