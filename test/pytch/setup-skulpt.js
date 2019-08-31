14b2c5bff80a9baa8c25e246bce1269cb3a0951f
Ben North <ben@redfrontdoor.org>
2019-08-31 14:55:14 +0100

    Extract common test setup code into own file
    It's still not entirely clear to me how the reqskulpt(false) call gets
    the "Sk" object to exist in the global namespace, but it seems to
    work.  My previous code might have worked more by fluke than design.

---
+++ test/pytch/setup-skulpt.js
@@ -0,0 +1,31 @@
+"use strict";
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+const fs = require("fs");
+const reqskulpt = require('../../support/run/require-skulpt').requireSkulpt;
+
+before(() => {
+    // Inject 'Sk' object into global namespace.
+    reqskulpt(false);
+
+    // Connect read/write to filesystem and stdout.
+    Sk.configure({
+        read: (fname) => { return fs.readFileSync(fname, "utf8"); },
+        output: (args) => { process.stdout.write(args); },
+    });
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    //
+    // Test helpers
+
+    global.import_from_local_file = function(fname) {
+        var code_text = fs.readFileSync(fname, { encoding: "utf8" });
+        return Sk.importMainWithBody(fname, false, code_text, true);
+    };
+
+    global.assert = require("assert");
+});
+
