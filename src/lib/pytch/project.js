fc84af551e9539bbe6996b06a4380c08acf0f2f6
Ben North <ben@redfrontdoor.org>
2019-08-31 12:46:53 +0100

    Add skeleton Python-level Project class

--- src/lib/pytch/project.js
+++ src/lib/pytch/project.js
@@ -6,5 +6,15 @@ var $builtinmodule = function (name) {
     const Project = function() {
     };
 
+    // Python-level "Project" class
+
+    const project_cls = function($gbl, $loc) {
+        $loc.__init__ = new Sk.builtin.func((self) => {
+            self.js_project = new Project();
+        });
+    };
+
+    mod.Project = Sk.misceval.buildClass(mod, project_cls, "Project", []);
+
     return mod;
 };
