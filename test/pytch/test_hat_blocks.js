8d3a6e709d6e96e00648d6b8c8a10b39e705aa76
Ben North <ben@redfrontdoor.org>
2019-08-31 14:31:27 +0100

    Add helpers for testing hat blocks

--- test/pytch/test_hat_blocks.js
+++ test/pytch/test_hat_blocks.js
@@ -32,4 +32,17 @@ const import_from_local_file = function(fname) {
 // Module 'pytch.hat_blocks'
 
 describe("pytch.hat_blocks module", function() {
+    const cls_method_handler_attribute = function(js_cls, js_method_name) {
+        var method = js_cls[js_method_name];
+        var py_handler_attr = method.$d._pytch_handler_for;
+        assert.ok(py_handler_attr);
+        return Sk.ffi.remapToJs(py_handler_attr);
+    };
+
+    const assert_handler_for = function(js_cls, js_method_name,
+                                        handler_type, handler_data) {
+        var handler = cls_method_handler_attribute(js_cls, js_method_name);
+        assert.equal(handler[0], handler_type);
+        assert.equal(handler[1], handler_data);
+    };
 });
