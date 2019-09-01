aa5421b69c6843b1e6207aaafc4c054c583f954f
Ben North <ben@redfrontdoor.org>
2019-09-01 16:18:27 +0100

    _yield_until_next_frame(): Add syscall

--- src/lib/pytch/syscalls.js
+++ src/lib/pytch/syscalls.js
@@ -1,5 +1,12 @@
 var $builtinmodule = function (name) {
     var mod = {};
 
+    mod._yield_until_next_frame = new Sk.builtin.func(() => {
+        var susp = new Sk.misceval.Suspension();
+        susp.resume = function() { return Sk.builtin.none.none$; };
+        susp.data = { type: "Pytch", subtype: "next-frame" };
+        return susp;
+    });
+
     return mod;
 };
