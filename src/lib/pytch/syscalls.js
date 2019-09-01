4b8a3459c98174944aa53e9a107360e044924c65
Ben North <ben@redfrontdoor.org>
2019-09-01 18:36:44 +0100

    _broadcast_and_wait(): Add syscall

--- src/lib/pytch/syscalls.js
+++ src/lib/pytch/syscalls.js
@@ -20,5 +20,17 @@ var $builtinmodule = function (name) {
         return susp;
     });
 
+    mod._broadcast_and_wait = new Sk.builtin.func((py_message) => {
+        var js_message = Sk.ffi.remapToJs(py_message);
+        var susp = new Sk.misceval.Suspension();
+        susp.resume = function() { return Sk.builtin.none.none$; };
+        susp.data = {
+            type: "Pytch",
+            subtype: "broadcast-and-wait",
+            subtype_data: js_message,
+        };
+        return susp;
+    });
+
     return mod;
 };
