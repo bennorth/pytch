61ed97de4a2cf701cfee23b1c65bd7deee13db17
Ben North <ben@redfrontdoor.org>
2019-09-01 21:07:05 +0100

    new_pytch_suspension(): Add helper function

--- src/lib/pytch/syscalls.js
+++ src/lib/pytch/syscalls.js
@@ -1,6 +1,13 @@
 var $builtinmodule = function (name) {
     var mod = {};
 
+    const new_pytch_suspension = function(subtype, data) {
+        var susp = new Sk.misceval.Suspension();
+        susp.resume = function() { return Sk.builtin.none.none$; };
+        susp.data = { type: "Pytch", subtype: subtype, subtype_data: data };
+        return susp;
+    };
+
     mod._yield_until_next_frame = new Sk.builtin.func(() => {
         var susp = new Sk.misceval.Suspension();
         susp.resume = function() { return Sk.builtin.none.none$; };
