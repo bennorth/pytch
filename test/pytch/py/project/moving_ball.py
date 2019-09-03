814c44bc7c6485d7980628f6964f4b6344d1483e
Ben North <ben@redfrontdoor.org>
2019-09-03 22:41:07 +0100

    Add simple moving ball test Py code

---
+++ test/pytch/py/project/moving_ball.py
@@ -0,0 +1,22 @@
+import pytch
+from pytch.hat_blocks import when_I_receive, when_green_flag_clicked
+from pytch.sprite import Sprite
+from pytch.project import Project
+
+
+class Ball(Sprite):
+    Costumes = {'yellow-ball': ('pytch-images/ball.png', 8, 8)}
+
+    def __init__(self):
+        Sprite.__init__(self)
+        self.go_to_xy(100, 50)
+        self.switch_costume('yellow-ball')
+        self.show()
+
+    @when_green_flag_clicked
+    def move(self):
+        self.change_x_pos(50)
+
+
+project = Project()
+project.register_sprite_class(Ball)
