df79e784d0f50376e02793d4e4492bd45df68227
Ben North <ben@redfrontdoor.org>
2019-09-04 00:59:19 +0100

    Provide useful things in top-level pytch module

--- src/lib/pytch/__init__.py
+++ src/lib/pytch/__init__.py
@@ -1,1 +1,18 @@
-# (This file intentionally left blank for now.)
+# Bring user-visible things in to top level with Scratch-friendly names.
+
+from pytch.sprite import Sprite
+
+from pytch.project import Project
+
+from pytch.hat_blocks import (
+    when_I_receive,
+    when_green_flag_clicked,
+)
+
+from pytch.syscalls import (
+    _broadcast as broadcast,
+    _broadcast_and_wait as broadcast_and_wait,
+    _sleep as wait_seconds,
+    _is_key_pressed as key_is_pressed,
+    _yield_until_next_frame as YNF,  ## Temporary until automatically inserted into loops
+)
