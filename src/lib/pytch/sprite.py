193444e9fc0df73a182ed04c7c8d684c83636ba0
Ben North <ben@redfrontdoor.org>
2019-09-03 22:25:34 +0100

    Sprite.__init__(): Assign basic properties
    Set up to be in the centre of the stage, at natural size, but not
    shown.

--- src/lib/pytch/sprite.py
+++ src/lib/pytch/sprite.py
@@ -1,2 +1,7 @@
 class Sprite:
-    pass
+    def __init__(self):
+        self._x = 0
+        self._y = 0
+        self._size = 1.0
+        self._shown = False
+        self._costume = 'no-such-costume';
