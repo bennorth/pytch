bb48755785c9319b1fefffee06c565fe4c524f81
Ben North <ben@redfrontdoor.org>
2019-09-04 00:53:43 +0100

    Add keypress-triggered methods to test case

--- test/pytch/py/project/moving_ball.py
+++ test/pytch/py/project/moving_ball.py
@@ -1,5 +1,5 @@
 import pytch
-from pytch.hat_blocks import when_I_receive, when_green_flag_clicked
+from pytch.hat_blocks import when_I_receive, when_green_flag_clicked, when_key_pressed
 from pytch.sprite import Sprite
 from pytch.syscalls import _sleep
 from pytch.project import Project
@@ -19,6 +19,14 @@ class Ball(Sprite):
         self.change_x_pos(50)
         _sleep(0.5)
         self.change_x_pos(60)
+
+    @when_key_pressed('w')
+    def move_up(self):
+        self.change_y_pos(10)
+
+    @when_key_pressed('s')
+    def move_down_lots(self):
+        self.change_y_pos(-100)
 
 
 project = Project()
