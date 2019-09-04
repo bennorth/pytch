24af93884501462abfda2343a97cf87e83896c23
Ben North <ben@redfrontdoor.org>
2019-09-04 01:00:11 +0100

    Add a few more Sprite methods

--- src/lib/pytch/sprite.py
+++ src/lib/pytch/sprite.py
@@ -6,6 +6,18 @@ class Sprite:
         self._shown = False
         self._costume = 'no-such-costume';
 
+    def x_pos(self):
+        return self._x
+
+    def set_x_pos(self, x):
+        self._x = x
+
+    def y_pos(self):
+        return self._y
+
+    def set_y_pos(self, y):
+        self._y = y
+
     def go_to_xy(self, x, y):
         self._x = x
         self._y = y
