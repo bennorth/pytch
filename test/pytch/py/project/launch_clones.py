5b601d782f9908b1fd61a4ec6ef4e76cc4c24f0b
Ben North <ben@redfrontdoor.org>
2019-09-08 21:35:06 +0100

    Add test Python for chain of clones

--- test/pytch/py/project/launch_clones.py
+++ test/pytch/py/project/launch_clones.py
@@ -37,5 +37,24 @@ class Alien(Sprite):
         pytch.create_clone_of(Alien)
 
 
+class Broom(Sprite):
+    Costumes = {}
+
+    def __init__(self):
+        Sprite.__init__(self)
+        self.copied_id = 1
+
+    @when_I_start_as_a_clone
+    def update_id(self):
+        self.copied_id += 1
+        if self.copied_id < 5:
+            pytch.create_clone_of(self)
+
+    @when_I_receive('clone-self')
+    def clone_self(self):
+        pytch.create_clone_of(self)
+
+
 project = Project()
 project.register_sprite_class(Alien)
+project.register_sprite_class(Broom)
