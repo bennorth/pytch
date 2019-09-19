570ca55abf7adf0427a1efed6f9accde6dba1027
Ben North <ben@redfrontdoor.org>
2019-09-20 00:08:17 +0100

    user_projects.set_project_name(): Add

--- example/pytch/pytch-gui.js
+++ example/pytch/pytch-gui.js
@@ -253,8 +253,16 @@ $(document).ready(function() {
                 user_projects_menu_header.removeClass("greyed-out");
         };
 
+        var set_project_name = function(name) {
+            user_project_name_input.val(name);
+        };
+
         refresh();
         save_my_project_button.click(save_project);
+
+        return {
+            set_project_name: set_project_name,
+        };
     })();
 
 
