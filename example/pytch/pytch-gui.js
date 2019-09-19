54b18a45c301ee43043748674e0fe74c54648a33
Ben North <ben@redfrontdoor.org>
2019-09-19 21:51:40 +0100

    Connect green-flag and red-stop to Pytch

--- example/pytch/pytch-gui.js
+++ example/pytch/pytch-gui.js
@@ -173,4 +173,17 @@ $(document).ready(function() {
                        keyboard: browser_keyboard,
                    },
                  });
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    //
+    // Connect browser events to Pytch handlers
+
+    $("#green-flag").click(() => {
+        Sk.pytch.current_live_project.js_project.on_green_flag_clicked();
+    });
+
+    $("#red-stop").click(() => {
+        Sk.pytch.current_live_project.js_project.on_red_stop_clicked();
+    });
 });
