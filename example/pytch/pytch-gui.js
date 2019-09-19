8077f5e4e96b12458f3ce3a7f55e888832c75336
Ben North <ben@redfrontdoor.org>
2019-09-19 21:12:38 +0100

    builtinRead(): Define
    Following example in main Skulpt code.

--- example/pytch/pytch-gui.js
+++ example/pytch/pytch-gui.js
@@ -8,4 +8,16 @@ $(document).ready(function() {
 
     var ace_editor = ace.edit("editor");
 
+
+    ////////////////////////////////////////////////////////////////////////////////
+    //
+    // Skulpt interaction
+
+    function builtinRead(x) {
+        if (Sk.builtinFiles === undefined
+                || Sk.builtinFiles["files"][x] === undefined)
+	    throw "File not found: '" + x + "'";
+
+        return Sk.builtinFiles["files"][x];
+    }
 });
