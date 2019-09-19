50b72260abca5d2c18e61fad44d48cae46ac1e79
Ben North <ben@redfrontdoor.org>
2019-09-19 21:21:38 +0100

    Implement render target interface via canvas

--- example/pytch/pytch-gui.js
+++ example/pytch/pytch-gui.js
@@ -24,6 +24,57 @@ $(document).ready(function() {
 
     ////////////////////////////////////////////////////////////////////////////////
     //
+    // Provide rendering target via canvas
+
+    const stage_canvas = (() => {
+        const canvas_elt = $("#pytch-canvas")[0];
+
+        if (! canvas_elt.hasAttribute("tabindex"))
+            canvas_elt.setAttribute("tabindex", 0);
+
+        const stage_wd = canvas_elt.width;
+        const stage_hwd = (stage_wd / 2) | 0;
+        const stage_ht = canvas_elt.height;
+        const stage_hht = (stage_ht / 2) | 0;
+
+        const canvas_ctx = canvas_elt.getContext("2d");
+
+        canvas_ctx.translate(stage_hwd, stage_hht);
+        canvas_ctx.scale(1, -1);
+
+        const enact_instructions = function(rendering_instructions) {
+            rendering_instructions.forEach(instr => {
+                switch(instr.kind) {
+                case "RenderImage":
+                    canvas_ctx.save();
+                    canvas_ctx.translate(instr.x, instr.y);
+                    canvas_ctx.scale(instr.scale, -instr.scale);
+                    canvas_ctx.drawImage(instr.image, 0, 0);
+                    canvas_ctx.restore();
+                    break;
+
+                default:
+                    throw new Error("unknown render-instruction kind \""
+                                    + instr.kind
+                                    + "\"");
+                }
+            });
+        };
+
+        const render = function(project) {
+            canvas_ctx.clearRect(-stage_hwd, -stage_hht, stage_wd, stage_ht);
+            enact_instructions(project.rendering_instructions());
+        };
+
+        return {
+            dom_elt: canvas_elt,
+            render: render,
+        };
+    })();
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    //
     // Provide 'keyboard' interface via browser keyboard
 
     const browser_keyboard = (() => {
