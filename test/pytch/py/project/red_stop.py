1f4ec3500ef048c676fde909c6693f1a4bf5e287
Ben North <ben@redfrontdoor.org>
2019-09-19 15:44:04 +0100

    Add proper test Python code for red-stop handling

--- test/pytch/py/project/red_stop.py
+++ test/pytch/py/project/red_stop.py
@@ -1,1 +1,22 @@
-pass
+import pytch
+
+from pytch.sprite import Sprite
+from pytch.project import Project
+from pytch.hat_blocks import when_I_receive
+
+
+class Broom(Sprite):
+    Costumes = {}
+
+    @when_I_receive('make-new-brooms')
+    def duplicate(self):
+        pytch.create_clone_of(self)
+
+    @when_I_receive('move')
+    def move(self):
+        while True:
+            self.change_x_pos(10)
+
+
+project = Project()
+project.register_sprite_class(Broom)
